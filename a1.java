0d8be8b552e6d50df83e95969f312c2170df95db
sarahzia-17 <sarahziaaa@gmail.com>
2025-09-07 22:30:18 +0500

    Assignment 1
    Assignment: Keep serving client untill the client exits(only one client at a time) using multi threading
    in java

---
+++ a1.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.*;
+
+class ClientHandler extends Thread {
+    private Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            out.println("Connected. Type exit to quit.");
+
+            String message;
+            while ((message = in.readLine()) != null) {
+                if (message.equalsIgnoreCase("exit")) {
+                    out.println("program exits");
+                    break;
+                }
+                out.println(message);
+            }
+
+            clientSocket.close();
+            System.out.println("Client disconnected");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class SingleClientServer {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            System.out.println("Server started");
+
+            while (true) {
+               
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("connected");
+              
+                ClientHandler handler = new ClientHandler(clientSocket);
+                handler.start();
+
+                handler.join();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
